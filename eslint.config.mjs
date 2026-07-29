d181962d20316139f169db34e0c70b7690afdc64
yadoran-2025 <jinyoung1571@naver.com>
2026-07-29 23:50:55 +0900

    chore: exclude linked worktrees from checks

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,9 @@ import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
 const eslintConfig = [
+  {
+    ignores: [".worktrees/**"],
+  },
   ...nextVitals,
   ...nextTs,
   {
